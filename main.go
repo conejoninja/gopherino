01861f6d2be993fecddad7727b05417156707487
Daniel Esteban <conejo@conejo.me>
2024-06-18 00:17:27 +0200

    added config for line following pins

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ import (
 )
 
 var (
-	d              int32
-	maqueen_hcsr04 hcsr04.Device
-	i2c            = machine.I2C0
-	maqueen_motor  *motor.Device
+	d                   int32
+	maqueen_hcsr04      hcsr04.Device
+	i2c                 = machine.I2C0
+	maqueen_motor       *motor.Device
+	lineLeft, lineRight machine.Pin
 )
 
 func main() {
@@ -22,6 +23,13 @@ func main() {
 	i2c.Configure(machine.I2CConfig{Frequency: machine.TWI_FREQ_100KHZ})
 	maqueen_motor = motor.New(i2c)
 	maqueen_motor.Configure()
+
+	// CONFIGURE PINS for the infrared grayscale sensor (line followers)
+	/*lineLeft = machine.P13
+	lineRight = machine.P14
+	lineLeft.Configure(machine.PinConfig{Mode: machine.PinInput})
+	lineRight.Configure(machine.PinConfig{Mode: machine.PinInput})*/
+
 	d = 123
 	for true {
 		d = maqueen_hcsr04.ReadDistance()
@@ -33,6 +41,7 @@ func main() {
 			maqueen_motor.Stop()
 			maqueen_motor.Forward()
 		}
+
 		time.Sleep(500 * time.Millisecond)
 	}
 }
